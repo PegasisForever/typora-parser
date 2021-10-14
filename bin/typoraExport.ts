a497f57278d76c6ab06542f57c72aaf6075af9b0
pegasis <me@pegasis.site>
2021-10-14 17:31:06 -0400

    command line export title defaults to file name

--- bin/typoraExport.ts
+++ bin/typoraExport.ts
@@ -2,6 +2,7 @@
 import {Command} from 'commander'
 import {version} from '../package.json'
 import fs from 'fs/promises'
+import path from 'path'
 import TyporaParser from '../src'
 import MathJaxRenderer from '../src/plugins/MathJaxRenderer'
 import HighlightJsRenderer from '../src/plugins/HighlightJsRenderer'
@@ -23,7 +24,7 @@ new Command()
   .requiredOption('-o, --output <file>', 'output file name')
   .option('-n, --vanilla-html', 'no typora-specific classes, corresponds to typora "export HTML (without styles)"')
   .option('-e, --exclude-head', 'don\'t include head and body tag')
-  .option('-t, --title <title>', 'title of the html, no effect when --exclude-head')
+  .option('-t, --title <title>', 'title of the html, no effect when --exclude-head, defaults to file name without extension')
   .option('-g, --extra-head-tags <file>', 'extra tags add to the head tag, no effect when --exclude-head')
   .option('-l, --code-display-line-numbers', 'show line numbers on code block, no effect when --vanilla-html')
   .option('-b, --math-auto-numbering', 'auto numbering math blocks')
@@ -33,7 +34,7 @@ new Command()
     const html = parseResult.renderHTML({
       vanillaHTML: options.vanillaHtml === true,
       includeHead: options.excludeHead !== true,
-      title: options.title === undefined ? null : options.title,
+      title: options.title === undefined ? path.parse(inputFileName).name : options.title,
       extraHeadTags: options.extraHeadTags ? await fs.readFile(options.extraHeadTags, {encoding: 'utf8'}) : null,
       latexRenderer: new MathJaxRenderer({
         autoNumbering: options.mathAutoNumbering === true,
